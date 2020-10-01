2baf17251709b7cae6bf60e9ea2c61138ae14da5
Chris Migut <cmiguthere@yahoo.com>
2020-10-01 18:33:31 -0400

    moved the hex logic to their own file locations

--- src/2_print_address.cpp
+++ src/2_print_address.cpp
@@ -27,91 +27,6 @@
 const uint8_t ROWS = 4;
 const uint8_t COLS = 20;
 
-
-// thought that i might had to convert binary value to hex string.. l2c lib already handles this
-// void binary_to_hex_converter(byte input_binary, char * output_hex, bit_order order){
-//   byte mask = 0xF0;
-
-//   if(order == high_order){
-//     input_binary &= mask;
-//   }
-
-//   if(order == low_order){
-//     input_binary <<= 4;
-//   }
-
-//   switch (input_binary)
-//   {
-//   case 0x10:
-//     *output_hex = '1';
-//     break;
-//   case 0x20:
-//     *output_hex = '2';
-//     break;
-//   case 0x30:
-//     *output_hex = '3';
-//     break;
-//   case 0x40:
-//     *output_hex = '4';
-//     break;
-//   case 0x50:
-//     *output_hex = '5';
-//     break;
-//   case 0x60:
-//     *output_hex = '6';
-//     break;
-//   case 0x70:
-//     *output_hex = '7';
-//     break;
-//   case 0x80:
-//     *output_hex = '8';
-//     break;
-//   case 0x90:
-//     *output_hex = '9';
-//     break;
-//   case 0xA0:
-//     *output_hex = 'A';
-//     break;
-//   case 0xB0:
-//     *output_hex = 'B';
-//     break;
-//   case 0xC0:
-//     *output_hex = 'C';
-//     break;
-//   case 0xD0:
-//     *output_hex = 'D';
-//     break;
-//   case 0xE0:
-//     *output_hex = 'E';
-//     break;
-//   case 0xF0:
-//     *output_hex = 'F';
-//     break; 
-//   }
-// }
-
-
-// char* binary_to_hex(byte input_byte){
-
-//   uint8_t hex_address_size = 5;
-//   char * hex_letter = (char*)malloc(sizeof(char));
-//   char * hex_address = (char*) malloc (sizeof(char) * hex_address_size);
-//   hex_address[0] = '0';
-//   hex_address[1] = 'x';
-
-//   binary_to_hex_converter(input_byte, hex_letter, high_order);
-//   hex_address[2] = *hex_letter;
-
-//   binary_to_hex_converter(input_byte, hex_letter, low_order);
-//   hex_address[3] = *hex_letter;
-//   hex_address[4] = '\0';
-
-//   free(hex_letter);
-//   Serial.print("I2C address found: ");
-//   Serial.println(hex_address);
-//   return hex_address;
-// }
-
 const byte address = find_i2c_address();
 LiquidCrystal_I2C lcd(address, COLS, ROWS);
 
@@ -120,24 +35,6 @@ void setup() {
   Wire.begin();
   Serial.begin(115200);
   Serial.println("\nI2C Scanner");
-
-  // byte l2c_address;
-  // int response_code = find_l2c_address(&l2c_address);
-  // switch (response_code)
-  // {
-  //   case address_found:
-  //     break;
-  //   case address_not_found:
-  //     Serial.println(responses[address_not_found].message);
-  //     exit(address_not_found);
-  //   case no_device_connected:
-  //     Serial.println(responses[no_device_connected].message);
-  //     exit(no_device_connected);
-  //   default:
-  //     Serial.println(responses[no_device_connected].message);
-  //     exit(no_device_connected);
-  // }
-
 
 
   lcd.init();
---
+++ src/binary_to_hex.h
@@ -0,0 +1,13 @@
+#ifndef BINARY_TO_HEX.H
+#define BINARY_TO_HEX.H
+
+#include "i2c_address_locator.h"
+
+enum bit_order {
+    high_order  = 0,
+    low_order   = 1 
+};
+
+char* binary_to_hex(byte input_byte);
+
+#endif
