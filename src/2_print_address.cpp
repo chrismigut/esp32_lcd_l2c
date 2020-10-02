a027475868b2188f30963c718291e2b3ff192ba0
Chris Migut <cmiguthere@yahoo.com>
2020-10-01 21:26:19 -0400

    removed unused code from main loop

--- src/2_print_address.cpp
+++ src/2_print_address.cpp
@@ -63,33 +63,4 @@ void setup() {
 }
 
 void loop() {
-  // byte error, address;
-  // int nDervices = 0;
-  // Serial.println("Scanner...");
-  
-  // for(address = 1; address < 127; address++){
-  //   Wire.beginTransmission(address);
-  //   error = Wire.endTransmission();
-  //   if(error == 0){
-  //     Serial.print("I2C device found at address 0x");
-  //     if(address < 16){
-  //       Serial.print("0");
-  //     }
-  //     Serial.println(address, HEX);
-  //     nDervices++;
-  //   }
-
-  //   if(error == 4){
-  //     Serial.print("Unknown error at address 0x");
-  //     if(address < 16){
-  //       Serial.print("0");
-  //     }
-  //   }
-  // }
-
-  // if(nDervices == 0){
-  //   Serial.println("No I2C devices found\n");
-  // }else{
-  //   Serial.println("done\n");
-  // }
 }
